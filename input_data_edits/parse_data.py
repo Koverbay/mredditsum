5bc530f5533e694ac4db14310164bb60b8d854cc
koverbay <keighley@vision.snu.kr>
2023-01-17 15:54:22 +0900

    fixed data parsing

--- input_data_edits/parse_data.py
+++ input_data_edits/parse_data.py
@@ -4,26 +4,39 @@ import json
 # data = [json.loads(row) for row in ]
 # print(len(data))
 
+def comment_to_string(comment):
+    if comment['author_anon'] == "OP":
+        author = "OP"
+    else:
+        author = f"Commenter {comment['author_anon'][-1]}"
+
+    string = f"{author}: {comment['body']}"
+    for reply in comment['replies']:
+        string += "\n"
+        string += comment_to_string(reply)
+    return string
+
 f = open('/gallery_tate/keighley.overbay/thread-summarization/thread_data/full_dataset/full_edited.json')
 data = json.load(f)
 
 data_dict = {}
 for thread in data['threads']:
     sub_id = thread["submission_id"]
-    caption = thread["caption"] + " [SEP] "
-    comments = []
-    for coms in thread["comments"]:
-        com_id = coms["comment_id"]
-        comments.append(coms['body'])
-        for replies in coms["replies"]:
-            comments.append(replies["body"])
+    caption = "OP: " + thread["raw_caption"] + " [SEP] "
+    comments = ""
+    for tl_com in thread["comments"]:
+        comments += "\n"
+        cts = comment_to_string(tl_com)
+        comments += cts
+        
+        # com_id = coms["comment_id"]
 
-    str_comms = " ".join(comments)
     data_dict[sub_id] = {
                 "sub_id": sub_id,
                 "caption": caption,
-                "cap_com": caption + str_comms,
+                "cap_com": caption + comments,
                 "edited_summaries": thread['edited_sum']
             }
 
-json.dump(data_dict, open("thread_caption_comments.json","w"))
+json.dump(data_dict, open("thread_caption_comments.json","w"), indent=4)
+
