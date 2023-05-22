ae29d9a2f709d5baf7d0f529e3f9b1ce25eb5268
koverbay <keighley@vision.snu.kr>
2023-05-22 18:22:32 +0900

    updated script

--- scripts/convert_to_datapoints.py
+++ scripts/convert_to_datapoints.py
@@ -10,45 +10,56 @@ def main(args):
         data = json.load(f)
         threads = data['threads']
     
+    processed_threads = []
+
+    if (args.prompt == 'imgcap') or (args.prompt == 'oponlyimgcap'):
+        with open('../data/image_captions_blip2_caption_best.json', 'r') as f:
+            imgcaps = json.load(f)
+
     for thread in threads:
         doc = ""
         doc_imgcap = ""
         # Add OP to document
         doc += "Original Post: "
-        doc += thread['raw_caption']
-
-        doc_imgcap += "Original Post: "
-        doc_imgcap += thread['raw_caption']
-
-        comment_number = 1
-        for tl_comment in thread['comments']:
-            cts = comment_to_string(tl_comment, str(comment_number))
-            doc += "\n"
-            doc += cts
-            doc_imgcap += "\n"
-            doc_imgcap += cts
-            comment_number += 1
+        doc += thread['raw_caption'].replace('\n', ' ').replace('\r', ' ')        
         
-        thread['document'] = doc
+        if (args.prompt == 'imgcap') or (args.prompt == 'oponlyimgcap') :
+            sub_id = thread['submission_id']
+            doc += f" Image: {imgcaps[sub_id]}."
+        doc += args.separator
+        
+        if (args.prompt != 'oponly') and (args.prompt != 'oponlyimgcap'):
+            for tl_comment in thread['comments']:
+                cts = comment_to_string(tl_comment, args.separator) #, str(comment_number))
+                doc += " "
+                doc += cts
+                doc += args.separator
+        processed_thread = {}
+        processed_thread['document'] = doc
+        if (args.prompt == 'oponly') or (args.prompt == 'oponlyimgcap'):
+            processed_thread['summary'] = thread["opsum"].replace('\n',' ').replace('\r', ' ')
+        else:
+            processed_thread['summary'] = thread["edited_sum"].replace('\n',' ').replace('\r', ' ')
+        processed_thread['id'] = thread["submission_id"]
+        processed_threads.append(processed_thread)
 
-        with open(savefile, 'w') as f:
-            json.dump(data, f, indent=4)
+    with open(savefile, 'w') as f:
+        json.dump({"threads": processed_threads}, f, indent=4)
 
-
-def comment_to_string(comment, comment_number):
-    string = f"Comment {str(comment_number)}: {comment['body']}"
+def comment_to_string(comment, sep):
+    string = f"{comment['author_anon']}: {comment['body']}".replace('\n',' ').replace('\r', ' ')
     if comment['replies'] == []:
         return string
-    new_comment_number = 1
     for reply in comment['replies']:
-        string += "\n"
-        string += comment_to_string(reply, (comment_number + '.' + str(new_comment_number)))
-        new_comment_number += 1
+        string += sep
+        string += comment_to_string(reply, sep)
     return string
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('-p', '--prompt', type=str, choices=['imgcap', 'glip', 'oponly', 'oponlyimgcap', 'none'], default='none', help="Type of prompt to add.")
+    parser.add_argument('-sep', '--separator', type=str, default="", help="The separator to go between each comment. Used for HT5 where the separator is |||.")
     parser.add_argument('-f', '--filename', type=str, help="Name of file to filter. Should be a thread json file.")
     parser.add_argument('-s', '--savefile', type=str, help="Name of file to save to. Should be a jsonfile")
 
