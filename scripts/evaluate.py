46bd6df84af39816f18654b50818c000155eccf5
koverbay <keighley@vision.snu.kr>
2023-05-25 15:24:34 +0900

    updated eval

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -10,12 +10,12 @@ def main(args):
     scores = files_rouge.get_scores(args.reference, args.hypothesis, avg=True)
     print(f"R1: {scores['rouge-1']['f']} \n R2: {scores['rouge-2']['f']} \n RL: {scores['rouge-l']['f']}")
     print("Computing BertScore...")
-    subprocess.run(f"bert-score -r {args.reference} {args.hypothesis} --lang en --rescale_with_baseline" ,shell=True)
+    subprocess.run(f"bert-score -r {args.reference} -c {args.hypothesis} --lang en --rescale_with_baseline" ,shell=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-h', '--hypothesis', type=str, help="Name of file with hypotheses.")
-    parser.add_argument('-r', '--reference', type=str, help="Name of file with references.")
+    parser.add_argument('-hyp', '--hypothesis', type=str, help="Name of file with hypotheses.")
+    parser.add_argument('-ref', '--reference', type=str, help="Name of file with references.")
     # parser.add_argument('-s', '--savefile', type=str, help="Name of file to save to. Should be a jsonfile")
     args = parser.parse_args()
     main(args)
